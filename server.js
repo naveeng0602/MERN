524a510c7200f591eb5b7b661e042086d7736995
Naveen Gupta <141839367+naveeng0602@users.noreply.github.com>
2023-10-21 00:56:12 +0530

    Update

--- server.js
+++ server.js
@@ -7,13 +7,17 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/catergoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
-import path from 'path'
+import path from 'path';
+import { fileURLTOPath } from 'url';
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object 
 const app = express();
